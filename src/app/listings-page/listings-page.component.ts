1435e64efa3b92c50da90971955c514fc8a1fb28
Tkumar02 <ta_265@yahoo.co.uk>
2023-04-15 09:13:53 +0100

    some additional changes

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Listing } from '../types';
-import { fakeListings } from '../fake-data';
-
+//import { fakeListings } from '../fake-data';
+import { Listing2, APIService } from '../API.service';
 @Component({
   selector: 'app-listings-page',
   templateUrl: './listings-page.component.html',
   styleUrls: ['./listings-page.component.css']
 })
 export class ListingsPageComponent implements OnInit {
-  listings: Listing[] = [];
+  //listings: Listing[] = [];
+  public listings2: Array<Listing2> = [];
 
-  constructor () { }
+  constructor (private api: APIService) { }
 
-  ngOnInit(): void {
-    this.listings = fakeListings;
+  // ngOnInit(): void {
+  //   this.listings = fakeListings;
+  // }
+
+  async ngOnInit() {
+    this.api.ListListing2s().then((event)=> {
+      this.listings2 = event.items as Listing2[];
+    })
   }
 
 }
